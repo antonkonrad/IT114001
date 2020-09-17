3595aa65e0a5e8ac1b1c3c4df98cf51864226d73
antonkonrad <ajk78@njit.edu>
2020-09-17 18:44:13 -0400

    java basic homework

---
+++ Basics/src/BasicHomeworkAssignment.java
@@ -0,0 +1,12 @@
+
+public class BasicHomeworkAssignment {
+	public static void main(String[] args) {
+		String name = "Timmothy";
+		System.out.println(name);
+		int num = 8;
+		System.out.println(num);
+		float floatnum = 4.1f;
+		System.out.println(floatnum);
+
+	}
+}
